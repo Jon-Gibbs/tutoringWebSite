16b8e0f76284fb2a1d0c397af1bb701bdddb8ce2
Jon <jon@gibbs.tk>
2024-09-26 12:58:34 -0700

    changed introduction

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
       <div classNme = "main-content">
         <h2>Who I Am</h2>
         <p className = 'main-text'>
-        Hello! My name is Jon Gibbs, a computer science student and private math tutor based in the greater Lynwood area. I got my start as a tutor by working in my college’s math help center, where I discovered my passion for helping students achieve their academic goals. With two years and hundreds of hours of one-on-one tutoring experience, I specialize in all math subjects up to the pre-calculus level, excluding geometry. My goal in private tutoring is to continue aiding students in their academic journey, and open doors to their future success.
+        Hello! I'm Jon Gibbs, a University of Washington Computer Science student and private math tutor based in the greater Lynwood area. I got my start as a tutor by working in my college’s math help center, where I discovered my passion for helping students achieve their academic goals. With two years and hundreds of hours of one-on-one tutoring experience, I specialize in all math subjects up to the pre-calculus level, excluding geometry. My goal in private tutoring is to continue aiding students in their academic journey, and open doors to their future success.
 
         </p>
       </div>
